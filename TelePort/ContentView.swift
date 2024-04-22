9d4d78921e276a12360858c07e523f3c17b3816e
Sepehr Rafiei <sepehrafiei@berkeley.edu>
2024-04-22 14:14:29 -0700

    Initial Commit

---
+++ TelePort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TelePort
+//
+//  Created by Sepehr Rafiei on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TelePort/TelePortApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelePortApp.swift
+//  TelePort
+//
+//  Created by Sepehr Rafiei on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TelePortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
